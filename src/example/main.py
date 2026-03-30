b22286dbe536bbd707022fbb78d3b4da0896b762
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-03-29 20:55:32 -0700

    noprint feature except on main but done differently

--- src/example/main.py
+++ src/example/main.py
@@ -5,12 +5,13 @@ from example.combine import combine_strings
 def make_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog="example", description="Combine strings!")
     parser.add_argument("strings", nargs="*")
+    parser.add_argument("--print", action="store_true")
     return parser
 
 def main(args=None):
     parser = make_parser()
     parsed_args = parser.parse_args(args)
 
-    result = combine_strings(*parsed_args.strings)
+    result = combine_strings(*parsed_args.strings, print_strings=parsed_args.print)
 
     print(result)
